b60df967b4558e317dc24a9e6bdf8b0dce8c3c43
safemetrica <tirany2014@gmail.com>
2026-05-02 16:37:50 +0000

    feat: NextAuth 카카오 로그인 추가

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,23 @@
+import NextAuth from "next-auth";
+import KakaoProvider from "next-auth/providers/kakao";
+
+export default NextAuth({
+  providers: [
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID!,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async session({ session, token }) {
+      if (session.user) {
+        (session.user as any).id = token.sub;
+      }
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/login",
+  },
+});
